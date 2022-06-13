3a6d2f32d13df65fa2ea9ae535a71bfa34df037a
Rob Townley itwooddesk <rob.townley+itwooddesk@gmail.com>
2022-06-13 01:37:14 -0500

    Created a mount point etc fstab bind /mnt/dsNas8/rsync-from-FreeNas8/FreeNas8-POOL0/rjt/bin/repos  /home/robert/repos -o bind,user,auto,exec Ok? How can the same file be committed and not staged for commit at the same time?
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   repos/jumpstartyadm.bash
    
     Changes not staged for commit:
    	modified:   repos/jumpstartyadm.bash

---
+++ repos/jumpstartyadm.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+git config --global --add user.name "Rob Townley"
+git config --global --add user.email rob.townley+${HOSTNAME}@gmail.com 
+
+if [[ ! -e ${HOME}/repos ]]; then mkdir ${HOME}/repos; fi;
+pushd ${HOME}/repos
+git clone https://github.com/TheLocehiliosan/yadm
+sudo cp /${HOME}/repos/yadm/yadm      /usr/local/bin/
+sudo cp /${HOME}/repos/yadm/bootstrap /usr/local/bin/
+sudo cp /${HOME}/repos/yadm/yadm.1    /usr/local/man/man1
+sudo cp /${HOME}/repos/yadm/completion/bash/yadm  /etc/bash_completion.d/
+#source ${HOME}/repos/yadm/completion/bash/yadm
+source /etc/bash_completion.d/yadm
+
+
+# robert@itwooddesk:~$ yadm init
+# hint: Using 'master' as the name for the initial branch. This default branch name
+# hint: is subject to change. To configure the initial branch name to use in all
+# hint: of your new repositories, which will suppress this warning, call:
+# hint: 
+# hint: 	git config --global init.defaultBranch <name>
+# hint: 
+# hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
+# hint: 'development'. The just-created branch can be renamed via this command:
+# hint: 
+# hint: 	git branch -m <name>
+# Initialized empty shared Git repository in /home/robert/.local/share/yadm/repo.git/
+
