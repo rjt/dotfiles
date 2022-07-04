f780d2e7e4c07edca5506647009e3d3a43a089b3
Rob Townley work-20211206-f34 <rob.townley+work-20211206-f34@gmail.com>
2022-07-04 02:03:05 -0500

    Moved some files under ~/bin/ to ~/.local/bin/ because ~/.local/bin/ was predifined in Fedora-34 .bashrc. Symbolic links for .tmate*.conf and for ~/.bashrc yadm alternates qualifiers ALL HAVE TO BE TRUE. distro.ubuntu and distro.Pop CANNOT BE TRUE AT SAME TIME. On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .bashrc##distro.CentOS
    	renamed:    .bashrc##distro.Fedora,distro.CentOS -> .bashrc##distro.Fedora
    	new file:   .bashrc##distro.Pop
    	renamed:    .bashrc##distro.ubuntu,distro.debian,distro.Pop -> .bashrc##distro.debian
    	new file:   .bashrc##distro.ubuntu
    	modified:   .bashrc##template.default
    	renamed:    .bash_aliases -> .bashrc.d/.bash_aliases
    	new file:   .bashrc.d/dotlocalbin.bash
    	modified:   .bashrc.d/starshiprc
    	new file:   .config/mimeapps.list
    	new file:   .config/starship.choman.toml
    	new file:   .config/starship.toml
    	new file:   .config/starship/nerd-font-symbols.toml
    	new file:   .config/starship/pastel-powerline.toml
    	new file:   .config/starship/plain-text-symbols.toml
    	new file:   .config/starship/starship-bug-report.txt
    	new file:   .config/starship/starship-print-config.toml
    	new file:   .config/starship/starship.choman.toml
    	new file:   .config/starship/strace-s90-o-starship.log
    	new file:   .local/bin/git-modified
    	renamed:    bin/GIT/git-push-annotated-datetime-tag-hmm.bash -> .local/bin/git-push-annotated-datetime-tag-hmm.bash
    	renamed:    bin/GIT/gitconfig-from-yadm-config -> .local/bin/gitconfig-from-yadm-config
    	renamed:    bin/GIT/yadm-push-annotated-datetime-tag-hmm.bash -> .local/bin/yadm-push-annotated-datetime-tag-hmm.bash
    	new file:   .tmate-corp.eceo.us.conf
    	new file:   .tmate-tmate.io.conf
    	modified:   .tmate.conf
    	deleted:    bin/pre-commit-config.yaml
    
    Changes not staged for commit:
    	deleted:    .config/starship.choman.toml

---
+++ .bashrc.d/dotlocalbin.bash
@@ -0,0 +1,7 @@
+# User specific environment
+# Ubuntu might not add ~/.local/bin to PATH.
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
