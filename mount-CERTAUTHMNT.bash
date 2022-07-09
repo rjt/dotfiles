0ddc4d443bc96b208c56fd993bea39bccddbb9d3
Rob Townley itwooddesk <rob.townley+itwooddesk@gmail.com>
2022-07-09 07:06:37 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    mount-CERTAUTHMNT.bash
    	new file:   sshmount.bash

--- mount-CERTAUTHMNT.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#fusermount 
-#sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT fuse.sshfs (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
-#sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
-sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT -o rw,nosuid,nodev,uid=1001,gid=1001
-#echo 'mount | egrep -i ssh >> ~/mount-CERTAUTHMNT.bash'
