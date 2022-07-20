b0a1bd22189d4962845bf45f37126e14bd0bb620
Rob Townley itwooddesk <rob.townley+itwooddesk@gmail.com>
2022-07-20 01:39:09 -0500

    Set all files under ~/.bashrc.d/ to be executable even though probably not necessary.  Forgot to add 01_set_win_title bash starship initial set terminal window title with $PWD via bash and starship. On branch main Your branch is ahead of 'origin/main' by 1 commit. (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   .bashrc.d/bash_aliases
    	modified:   .bashrc.d/yadmaliases
    	new file:   .ssh/config
    	modified:   sshmount.bash
    
    Changes not staged for commit:
    	modified:   .config/mimeapps.list

--- sshmount.bash
+++ sshmount.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #fusermount 
 SSHFSMNTPNT=$1
 if [[   -z $SSHFSMNTPNT ]]; then SSHFSMNTPNT=~/CERTAUTHMNT/; fi;
@@ -7,7 +7,7 @@ if [[ ! -d $SSHFSMNTPNT ]]; then mkdir $SSHFSMNTPNT; fi;
 #sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT fuse.sshfs (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
 #sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
 #sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ~/CERTAUTHMNT -o rw,nosuid,nodev,uid=1001,gid=1001
-sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ${SSHFSMNTPNT} -o rw,nosuid,nodev,uid=1001,gid=1001
+sshfs rjt@certauth.network.corp.eceo.us:/home/rjt/CertTemp ${SSHFSMNTPNT} -o rw,nosuid,nodev,uid=1000,gid=1001
 if [[ -x ${SSHFSMNTPNT} ]]; then 
  	if [[ ! -O ${SSHFSMNTPNT}/ECEO_ROOT_CA_20200426.cacert.pub.pem ]]; then 
 	  echo "With NFS, different UID:GID breaks things.  ";
@@ -24,6 +24,7 @@ echo "          umount ${SSHFSMNTPNT}         # OS X, FreeBSD"
  
 
 mount | egrep -i '(ssh|fuse)'
+echo "IF FAILS TO MOUNT, try:  fusermount3 -u ${SSHFSMNTPNT}"
 # fusectl on /sys/fs/fuse/connections type fusectl (rw,nosuid,nodev,noexec,relatime)
 # gvfsd-fuse on /run/user/1000/gvfs type fuse.gvfsd-fuse (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
 # portal on /run/user/1000/doc type fuse.portal (rw,nosuid,nodev,relatime,user_id=1000,group_id=1000)
